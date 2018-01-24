4f1e1fc13c080cbea3e80cbd6f258171a9da47a1
Austin Hill <austin@sparkaerial.com>
2018-01-24 11:55:28 -0800

    fixed sph imports in x16r

--- x16r.c
+++ x16r.c
@@ -4,22 +4,22 @@
 #include <string.h>
 #include <stdio.h>
 
-#include "sph/sph_blake.h"
-#include "sph/sph_bmw.h"
-#include "sph/sph_groestl.h"
-#include "sph/sph_jh.h"
-#include "sph/sph_keccak.h"
-#include "sph/sph_skein.h"
-#include "sph/sph_luffa.h"
-#include "sph/sph_cubehash.h"
-#include "sph/sph_shavite.h"
-#include "sph/sph_simd.h"
-#include "sph/sph_echo.h"
-#include "sph/sph_hamsi.h"
-#include "sph/sph_fugue.h"
-#include "sph/sph_shabal.h"
-#include "sph/sph_whirlpool.h"
-#include "sph/sph_sha2.h"
+#include "sha3/sph_blake.h"
+#include "sha3/sph_bmw.h"
+#include "sha3/sph_groestl.h"
+#include "sha3/sph_jh.h"
+#include "sha3/sph_keccak.h"
+#include "sha3/sph_skein.h"
+#include "sha3/sph_luffa.h"
+#include "sha3/sph_cubehash.h"
+#include "sha3/sph_shavite.h"
+#include "sha3/sph_simd.h"
+#include "sha3/sph_echo.h"
+#include "sha3/sph_hamsi.h"
+#include "sha3/sph_fugue.h"
+#include "sha3/sph_shabal.h"
+#include "sha3/sph_whirlpool.h"
+#include "sha3/sph_sha2.h"
 
 #define X16R_BLAKE      0
 #define X16R_BMW        1
